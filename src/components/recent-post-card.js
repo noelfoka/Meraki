fd7725b1f48c9c0ddcb8f3d6cb9f314dfb9e9bcb
noelfoka <nomgnenoel@gmail.com>
2023-02-19 13:29:43 +0100

    button center

---
+++ src/components/recent-post-card.js
@@ -0,0 +1,20 @@
+import Button from "react-bootstrap/Button";
+
+function RecentPostCard() {
+  return (
+    <div>
+      <Button
+        type="button"
+        className="btn btn-secondary text-center items-center"
+        id="div1"
+        variant="info"
+        size=""
+        active
+      >
+        Laravel
+      </Button>
+    </div>
+  );
+}
+
+export default RecentPostCard;

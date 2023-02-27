295c079491b2850b6d842c6b90e59b708935bdd0
noelfoka <nomgnenoel@gmail.com>
2023-02-27 20:47:02 +0100

    final navbar

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -27,40 +27,27 @@ function NavBar() {
         </Navbar.Collapse>
       </Container>
     </Navbar> */}
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="lg" className="shadow-sm">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Brand href="#">Meraki UI</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
-            </Nav>
+          <Navbar.Collapse
+            id="navbarScroll"
+            className="me-auto w-100 d-flex justify-content-end"
+          >
             <Form className="d-flex">
-              <Form.Control
+              {/* <Form.Control
                 type="search"
                 placeholder="Search"
                 className="me-2"
                 aria-label="Search"
               />
-              <Button variant="outline-success">Search</Button>
+              <Button variant="outline-success">Search</Button> */}
+              <Nav className="">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Blog</Nav.Link>
+                <Nav.Link href="#pricing">About Us</Nav.Link>
+              </Nav>
             </Form>
           </Navbar.Collapse>
         </Container>
